52b5eda7b2b6c77f85bd41837c5e2e4e71ef66ef
PeterGawecki <pgawecki@hotmail.com>
2018-06-04 14:30:17 -0400

    added all functionality listed in README

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('testing');
+console.log('testing')
+
+const button = document.getElementById('button')
+
+
+const changeText = function(){
+    const heading = document.getElementById('hd2')
+    const form = document.getElementById('box');
+    heading.textContent = form.value;
+}
+
+document.getElementById('box').onkeypress = function(event){
+    const keyCode = event.keyCode
+    if (keyCode === 13){
+        const heading = document.getElementById('hd2')
+        const form = document.getElementById('box')
+        heading.textContent = form.value
+        return false //why is this necessary?
+    }
+  }
+
+button.addEventListener('click', changeText)
