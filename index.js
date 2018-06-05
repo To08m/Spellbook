47ad7dbec9a0460733c1c2fc9724e19a126748ae
PeterGawecki <pgawecki@hotmail.com>
2018-06-05 10:53:12 -0400

    add items from the form to a div and clears form after each input

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ console.log('testing')
 const button = document.querySelector('button')
 
 const changeText = function(){
-    const heading = document.querySelector('h2')
+    const heading = document.querySelector('#spells')
     const form = document.querySelector('form')//proper selector?
-    heading.textContent = form.spellName.value
+    heading.textContent = ' ' + form.spellName.value
 }
 
 document.querySelector('input').onkeypress = function(event){
     const keyCode = event.keyCode
     if (keyCode === 13){
-        const heading = document.querySelector('h2')
+        const heading = document.querySelector('#spells')
         const form = document.querySelector('form')
-        heading.textContent = form.spellName.value
+        heading.textContent += ' ' + form.spellName.value
+        form.spellName.value = ''
         return false
     }
   }
