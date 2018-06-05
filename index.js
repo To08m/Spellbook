74ccee884544eea748988bde935abb32493519f0
PeterGawecki <pgawecki@hotmail.com>
2018-06-05 10:28:23 -0400

    created working copy with proper specifiers

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const button = document.querySelector('button')
 
 const changeText = function(){
     const heading = document.querySelector('h2')
-    const form = document.querySelector('input');//proper selector?
-    heading.textContent = form.value;
-    return false
+    const form = document.querySelector('form')//proper selector?
+    heading.textContent = form.spellName.value
 }
 
 document.querySelector('input').onkeypress = function(event){
     const keyCode = event.keyCode
     if (keyCode === 13){
         const heading = document.querySelector('h2')
-        const form = document.querySelector('input')
-        heading.textContent = form.value
+        const form = document.querySelector('form')
+        heading.textContent = form.spellName.value
         return false
     }
   }
