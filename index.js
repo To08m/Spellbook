7df7d10c0f3d8affbc09b6cf53e84dec1da1b68e
PeterGawecki <pgawecki@hotmail.com>
2018-06-05 11:13:48 -0400

    add spells to an unordered list

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log('testing')
 const button = document.querySelector('button')
 
 const changeText = function(){
-    const heading = document.querySelector('#spells')
+    const heading = document.querySelector('#list')
     const form = document.querySelector('form')
     heading.innerHTML += `<p>${form.spellName.value}</p>` //'<p>'+ form.spellName.value +'</p>'
     //form.spellName.value = ''
@@ -13,9 +13,9 @@ const changeText = function(){
 document.querySelector('input').onkeypress = function(event){
     const keyCode = event.keyCode
     if (keyCode === 13){
-        const heading = document.querySelector('#spells')
+        const heading = document.querySelector('#list')
         const form = document.querySelector('form')
-        heading.innerHTML += '<p>'+ form.spellName.value +'</p>'
+        heading.innerHTML += '<li>'+ form.spellName.value +'</li>'
         form.reset()
         return false
     }
