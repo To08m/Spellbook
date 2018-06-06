ab6594fc584dd66f9d4c6f8cd7b47b3fd28b2051
PeterGawecki <pgawecki@hotmail.com>
2018-06-06 11:28:57 -0400

    example of multiple functions with span

--- index.js
+++ index.js
@@ -27,15 +27,49 @@ const addSpell = function(ev) {
   /*spans with createElement and appendChild
      const nameSpan = document.createElement('span')
      nameSpan.textContent = spellName
+     nameSpan.classList.add('spellName')
 
      const levelSpan = document.createElement('span')
      levelSpan.textContent = level
+     levelSpan.classList.add('level')
 
      const item = document.createElement('li')
+     item.classList.add('spell')
      item.appendChild(nameSpan)
      item.appendChild(levelSpan)
 
      list.appendChild(item)
+  */
+
+  /*
+  const renderProperty = function(name, value) {
+  const el = document.createElement('span')
+  el.textContent = value
+  el.classList.add(name)
+  return el
+}
+
+const handleSubmit = function(ev) {
+  ev.preventDefault()
+
+  const f = ev.target
+  const spellName = f.spellName.value
+  const level = f.level.value
+
+  const list = document.querySelector('#spells')
+
+  const nameSpan = renderProperty('spellName', spellName)
+  const levelSpan = renderProperty('level', level)
+
+  const item = document.createElement('li')
+  item.classList.add('spell')
+  item.appendChild(nameSpan)
+  item.appendChild(levelSpan)
+
+  list.appendChild(item)
+
+  f.reset()
+}
   */
 }
 
