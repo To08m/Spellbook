dc0362421927051280a6730312843bd9af58e021
PeterGawecki <pgawecki@hotmail.com>
2018-06-06 14:31:32 -0400

    updated all files for homework3

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # Spellbook
 
+## Day 3 Homework
+
+* In addition to building a list item and adding it to the DOM (as we are now), also store each spell in an array.
+
+### Bonus Credit
+
+* Add a delete button to each list item that removes it from the list.
+
+##$ Super Mega Bonus Credit
+
+*Remove the item from the array as well.
+
+
 ## Day 2 HW 
 
 * Add a second field of your choice to the form.
 
-## Bonus Cred
+### Bonus Cred
 
 * Lookup document.createElement and appendChild, and try adding the list items that way instead of with innerHTML
 
-## Super mega bonus cred
+### Super mega bonus cred
 
 * Display each value in the list item(name,etc) in seperate elements and style them differently somehow.
 
@@ -20,7 +33,7 @@
 </li>
 ```
 
-## Super mega bonus credit hyper fighting 
+### Super mega bonus credit hyper fighting 
 
 * Build each new element (the `li`, each `span`, etc.) in separate functions.
 
@@ -28,16 +41,16 @@
 
 * Make the button change the text of the heading ('h1').
 
-## Bonus Cred 
+### Bonus Cred 
 
 * Add multiple headings to the page, and make the button change to the second one(Use class or id).
 
-## Super mega bonus cred
+### Super mega bonus cred
 
 * add a form to the page
 * Add a text input to the form
 * When the form is submitted, update the heading with the text that you type in as text input.
 
-## Super meg bonus Cred hype fight
+### Super meg bonus Cred hype fight
 
 * Make sure it works when you press enter on the keyboard not just when click
--- index.js
+++ index.js
@@ -1,47 +1,5 @@
 const form = document.querySelector('form')
 
-
-const addSpell = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-
-  const li1 = document.createElement("LI")
-  
-  const spellName = document.createTextNode(f.spellName.value)
-
-  li1.appendChild(spellName)
-
-  const spellsList = document.querySelector('#spells')
-
-  spellsList.appendChild(li1)
-
-  /*spans with innerHTML
-  spellsDiv.innerHTML += `
-    <li>
-      <span class="spellName">${spellName}</span>,
-      <span class="level">lvl ${level}</span>
-    </li>
-  `
-  */
-
-  /*spans with createElement and appendChild
-     const nameSpan = document.createElement('span')
-     nameSpan.textContent = spellName
-     nameSpan.classList.add('spellName')
-
-     const levelSpan = document.createElement('span')
-     levelSpan.textContent = level
-     levelSpan.classList.add('level')
-
-     const item = document.createElement('li')
-     item.classList.add('spell')
-     item.appendChild(nameSpan)
-     item.appendChild(levelSpan)
-
-     list.appendChild(item)
-  */
-
-  /*
 const renderProperty = function(name, value) {
   const el = document.createElement('span')
   el.textContent = value
@@ -87,41 +45,5 @@ const handleSubmit = function(ev) {
 
   f.reset()
 }
-  */
-}
 
-const addMana = function(ev){
-    ev.preventDefault()
-    const f = ev.target
-
-    const li2 = document.createElement("LI")
-
-    const manaCost = document.createTextNode(f.manaCost.value)
-
-    li2.appendChild(manaCost)
-
-    const manaList = document.querySelector('#cost')
-
-    manaList.appendChild(li2)
-}
-
-const addPow = function(ev){
-    ev.preventDefault()
-    const f = ev.target
-
-    const li3 = document.createElement("LI")
-
-    const powVal = document.createTextNode(f.power.value)
-
-    li3.appendChild(powVal)
-
-    const powList = document.querySelector('#str')
-
-    powList.appendChild(li3)
-
-    f.reset()
-}
-
-form.addEventListener("submit",addSpell)
-form.addEventListener("submit",addMana)
-form.addEventListener("submit",addPow)
+form.addEventListener('submit', handleSubmit)
